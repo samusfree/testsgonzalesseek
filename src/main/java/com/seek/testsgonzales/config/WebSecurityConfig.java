06929c00b5b0293a852c7c367d3f1073b151da05
Samuel Gonzales <sagonzales89@gmail.com>
2024-09-28 03:16:27 -0500

    Actualizar API web con nuevas rutas, controladores y configuración de JWT en Spring Security

--- src/main/java/com/seek/testsgonzales/config/WebSecurityConfig.java
+++ src/main/java/com/seek/testsgonzales/config/WebSecurityConfig.java
@@ -1,22 +1,77 @@
 package com.seek.testsgonzales.config;
 
+import com.seek.testsgonzales.security.filter.JWTRequestFilter;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@SecurityScheme(
+    name = "Bearer Authentication",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+  private final JWTRequestFilter jwtRequestFilter;
+
+  public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+    this.jwtRequestFilter = jwtRequestFilter;
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsManager() {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    UserDetails user = User.builder().username("user").password(encoder.encode("password")).build();
+
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(InMemoryUserDetailsManager userDetailsManager) {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsManager);
+    return new ProviderManager(provider);
+  }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
-        .cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
-        .httpBasic(Customizer.withDefaults());
+    http
+        .authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/api/v1/login",
+                "swagger-ui.html", "swagger-ui/**", "/v3/api-docs/**").permitAll())
+        .authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/swagger-ui.html")
+                .permitAll())
+        .authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(HttpMethod.DELETE,
+                "/api/v1/candidate/**").permitAll())
+        .authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(HttpMethod.PUT,
+                "/api/v1/candidate/**").permitAll())
+        .authorizeHttpRequests(
+            (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers("/api/v1/candidate",
+                "/api/v1/candidates/**").authenticated()).cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable);
+    http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
 }
--- src/main/java/com/seek/testsgonzales/delegate/SecurityAPIDelegateImpl.java
+++ src/main/java/com/seek/testsgonzales/delegate/SecurityAPIDelegateImpl.java
@@ -3,14 +3,36 @@ package com.seek.testsgonzales.delegate;
 import com.seek.testsgonzales.api.SecurityAPIDelegate;
 import com.seek.testsgonzales.model.GenericResponse;
 import com.seek.testsgonzales.model.LoginRequest;
+import com.seek.testsgonzales.util.JWTUtil;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityAPIDelegateImpl implements SecurityAPIDelegate {
 
+  private final AuthenticationManager authenticationManager;
+  private final JWTUtil jwtUtil;
+
+  public SecurityAPIDelegateImpl(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+    this.authenticationManager = authenticationManager;
+    this.jwtUtil = jwtUtil;
+  }
+
   @Override
   public ResponseEntity<GenericResponse<String>> login(LoginRequest loginRequest) {
-    return null;
+    try {
+      authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(loginRequest.user(), loginRequest.password())
+      );
+    } catch (AuthenticationException e) {
+      throw new RuntimeException("Incorrect username or password", e);
+    }
+
+    final String jwt = jwtUtil.generateToken(loginRequest.user());
+
+    return ResponseEntity.ok(GenericResponse.<String>builder().success(true).data(jwt).build());
   }
 }
